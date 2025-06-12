b77c1d8750489ec13e6da95f3709bee2737890af
Tejas <tejasgiri910@gmail.com>
2025-06-12 14:31:29 +0530

    feat(agents): integrate @tanstack/react-table for agent data display and add EmptyState component

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -6,19 +6,23 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter,  protectedProcedure } from "@/trpc/init";
 import { agentInsertSchema } from "../schema";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 
 
 
 export const agentRouter = createTRPCRouter({
     getMany: protectedProcedure.query(async () => {
-        return  await db.select().from(agents)
+        return  await db.select({ ...getTableColumns(agents), 
+            //TODO change actual count
+            meetingCounts: sql<number>`5`}).from(agents)
         
 
     }),
     getOne: protectedProcedure.input(z.object({id:z.string()})).query(async ({input}) => {
-        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id))
+        const [existingAgent] = await db.select({ ...getTableColumns(agents), 
+            //TODO change actual count
+            meetingCounts: sql<number>`5`}).from(agents).where(eq(agents.id, input.id))
         return existingAgent;
 
     }),
